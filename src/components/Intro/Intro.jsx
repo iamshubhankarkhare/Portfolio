b464a3833068bb0b77b8d2f6ac6b0b508aa24d43
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2020-05-14 23:55:38 +0530

    3D initial

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Intro.module.css'
+
+function Intro() {
+    return (
+        <div className={styles.container}>
+            <button > Click me mf </button>
+
+        </div>
+    )
+}
+
+export default Intro
